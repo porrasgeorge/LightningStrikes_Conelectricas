aef8e97eb101615f28f3c85d6a2cc13f330b4d71
jporras <jporras@conelectricas.com>
2018-10-31 12:07:52 -0600

    primer

--- LightningStrikes_Conelectricas/Form1.Designer.cs
+++ LightningStrikes_Conelectricas/Form1.Designer.cs
@@ -29,12 +29,219 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dtp_Inicial = new System.Windows.Forms.DateTimePicker();
+            this.dtp_final = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cb_cooperativa = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgv_lightning1 = new System.Windows.Forms.DataGridView();
+            this.lightningStrikesDataSet = new LightningStrikes_Conelectricas.LightningStrikesDataSet();
+            this.getLightningsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.getLightningsTableAdapter = new LightningStrikes_Conelectricas.LightningStrikesDataSetTableAdapters.GetLightningsTableAdapter();
+            this.cantidadDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.latitudDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.longitudDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_createKML = new System.Windows.Forms.Button();
+            this.Check3D = new System.Windows.Forms.CheckBox();
+            this.btn_Consultar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_lightning1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightningStrikesDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getLightningsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LightningStrikes_Conelectricas.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(379, 82);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dtp_Inicial
+            // 
+            this.dtp_Inicial.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtp_Inicial.Location = new System.Drawing.Point(124, 149);
+            this.dtp_Inicial.Name = "dtp_Inicial";
+            this.dtp_Inicial.Size = new System.Drawing.Size(104, 20);
+            this.dtp_Inicial.TabIndex = 1;
+            this.dtp_Inicial.Value = new System.DateTime(2018, 1, 1, 0, 0, 0, 0);
+            // 
+            // dtp_final
+            // 
+            this.dtp_final.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtp_final.Location = new System.Drawing.Point(124, 175);
+            this.dtp_final.Name = "dtp_final";
+            this.dtp_final.Size = new System.Drawing.Size(104, 20);
+            this.dtp_final.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Fecha Inicial";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Fecha Final";
+            // 
+            // cb_cooperativa
+            // 
+            this.cb_cooperativa.FormattingEnabled = true;
+            this.cb_cooperativa.Location = new System.Drawing.Point(124, 201);
+            this.cb_cooperativa.Name = "cb_cooperativa";
+            this.cb_cooperativa.Size = new System.Drawing.Size(104, 21);
+            this.cb_cooperativa.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Cooperativa";
+            // 
+            // dgv_lightning1
+            // 
+            this.dgv_lightning1.AllowUserToDeleteRows = false;
+            this.dgv_lightning1.AutoGenerateColumns = false;
+            this.dgv_lightning1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_lightning1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.cantidadDataGridViewTextBoxColumn,
+            this.latitudDataGridViewTextBoxColumn,
+            this.longitudDataGridViewTextBoxColumn});
+            this.dgv_lightning1.DataSource = this.getLightningsBindingSource;
+            this.dgv_lightning1.Location = new System.Drawing.Point(19, 283);
+            this.dgv_lightning1.Name = "dgv_lightning1";
+            this.dgv_lightning1.ReadOnly = true;
+            this.dgv_lightning1.Size = new System.Drawing.Size(372, 438);
+            this.dgv_lightning1.TabIndex = 7;
+            // 
+            // lightningStrikesDataSet
+            // 
+            this.lightningStrikesDataSet.DataSetName = "LightningStrikesDataSet";
+            this.lightningStrikesDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // getLightningsBindingSource
+            // 
+            this.getLightningsBindingSource.DataMember = "GetLightnings";
+            this.getLightningsBindingSource.DataSource = this.lightningStrikesDataSet;
+            // 
+            // getLightningsTableAdapter
+            // 
+            this.getLightningsTableAdapter.ClearBeforeFill = true;
+            // 
+            // cantidadDataGridViewTextBoxColumn
+            // 
+            this.cantidadDataGridViewTextBoxColumn.DataPropertyName = "Cantidad";
+            this.cantidadDataGridViewTextBoxColumn.HeaderText = "Cantidad";
+            this.cantidadDataGridViewTextBoxColumn.Name = "cantidadDataGridViewTextBoxColumn";
+            this.cantidadDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // latitudDataGridViewTextBoxColumn
+            // 
+            this.latitudDataGridViewTextBoxColumn.DataPropertyName = "Latitud";
+            this.latitudDataGridViewTextBoxColumn.HeaderText = "Latitud";
+            this.latitudDataGridViewTextBoxColumn.Name = "latitudDataGridViewTextBoxColumn";
+            this.latitudDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // longitudDataGridViewTextBoxColumn
+            // 
+            this.longitudDataGridViewTextBoxColumn.DataPropertyName = "Longitud";
+            this.longitudDataGridViewTextBoxColumn.HeaderText = "Longitud";
+            this.longitudDataGridViewTextBoxColumn.Name = "longitudDataGridViewTextBoxColumn";
+            this.longitudDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // btn_createKML
+            // 
+            this.btn_createKML.Location = new System.Drawing.Point(287, 172);
+            this.btn_createKML.Name = "btn_createKML";
+            this.btn_createKML.Size = new System.Drawing.Size(75, 23);
+            this.btn_createKML.TabIndex = 8;
+            this.btn_createKML.Text = "Crear KML";
+            this.btn_createKML.UseVisualStyleBackColor = true;
+            this.btn_createKML.Click += new System.EventHandler(this.btn_Consultar_Click);
+            // 
+            // Check3D
+            // 
+            this.Check3D.AutoSize = true;
+            this.Check3D.Location = new System.Drawing.Point(124, 228);
+            this.Check3D.Name = "Check3D";
+            this.Check3D.Size = new System.Drawing.Size(40, 17);
+            this.Check3D.TabIndex = 9;
+            this.Check3D.Text = "3D";
+            this.Check3D.UseVisualStyleBackColor = true;
+            // 
+            // btn_Consultar
+            // 
+            this.btn_Consultar.Location = new System.Drawing.Point(287, 145);
+            this.btn_Consultar.Name = "btn_Consultar";
+            this.btn_Consultar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Consultar.TabIndex = 10;
+            this.btn_Consultar.Text = "Consultar";
+            this.btn_Consultar.UseVisualStyleBackColor = true;
+            this.btn_Consultar.Click += new System.EventHandler(this.btn_Consultar_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(408, 775);
+            this.Controls.Add(this.btn_Consultar);
+            this.Controls.Add(this.Check3D);
+            this.Controls.Add(this.btn_createKML);
+            this.Controls.Add(this.dgv_lightning1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cb_cooperativa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtp_final);
+            this.Controls.Add(this.dtp_Inicial);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Descargas Atmosféricas";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_lightning1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lightningStrikesDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getLightningsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DateTimePicker dtp_Inicial;
+        private System.Windows.Forms.DateTimePicker dtp_final;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cb_cooperativa;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgv_lightning1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cantidadDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn latitudDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn longitudDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource getLightningsBindingSource;
+        private LightningStrikesDataSet lightningStrikesDataSet;
+        private LightningStrikesDataSetTableAdapters.GetLightningsTableAdapter getLightningsTableAdapter;
+        private System.Windows.Forms.Button btn_createKML;
+        private System.Windows.Forms.CheckBox Check3D;
+        private System.Windows.Forms.Button btn_Consultar;
     }
 }
 
--- LightningStrikes_Conelectricas/Properties/Settings.Designer.cs
+++ LightningStrikes_Conelectricas/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LightningStrikes_Conelectricas.Properties
-{
-
-
+namespace LightningStrikes_Conelectricas.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=192.168.4.2;Initial Catalog=LightningStrikes;Persist Security Info=Tr" +
+            "ue;User ID=jorge;Password=ERR100189")]
+        public string LightningStrikesConnectionString {
+            get {
+                return ((string)(this["LightningStrikesConnectionString"]));
             }
         }
     }
